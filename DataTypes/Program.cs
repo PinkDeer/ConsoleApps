fd76f33b9370c94a9cd9fd1a272e793009686a6c
PinkDeer <andreyzinovets@gmail.com>
2019-11-25 14:31:01 +0300

    Initial commit

---
+++ DataTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Тип {0.8} |\t.NET {1.8} |\tРазмер {2} \t | MIN = {3.3} | max = {4}";
+            Console.Title = "Типы данных в С#";
+        }
+    }
+}
