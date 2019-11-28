8ee90269b2609bd5b0b91f4af16e9152a729d773
PinkDeer <andreyzinovets@gmail.com>
2019-11-28 15:40:59 +0300

    Add variables

---
+++ variables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = 5;
+            Console.WriteLine(a);
+
+            int b = 7;
+            Console.WriteLine(b);
+
+            int c, d;
+            c = 6;
+            d = 12;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+            int Var = 2;
+            int var = 1;
+
+            Console.WriteLine(Var);
+            Console.WriteLine(var);
+
+            //Два раз невозможно объявить переменную
+            //int = x;
+            //int = x;
+
+            Console.ReadLine();
+        }
+    }
+}
