3fd301236b267ceb6f6b1ecc67af950bacec105b
PinkDeer <andreyzinovets@gmail.com>
2019-11-29 18:53:26 +0300

    Add While

---
+++ While/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Цикл While";
+            int limit = int.Parse(Console.ReadLine());
+                
+            int count = 0; 
+            while (count < limit)
+            {
+                count++;
+                Console.WriteLine(count);
+            }
+            Console.ReadKey();
+        }
+    }
+}
