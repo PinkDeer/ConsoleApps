b23275a5b4e19d335c0c72636b3ac757cf5330f2
PinkDeer <andreyzinovets@gmail.com>
2019-11-29 13:16:36 +0300

    Add ConditionalExpressions

---
+++ ConditionalExpressions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace ConditionalExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Условные выражения";
+
+            // Опраторы сравнения
+
+            int a = 4;
+            int b = 5;
+
+            bool resault = a == b;
+            Console.WriteLine(resault);
+
+            Console.WriteLine(a != b);
+            Console.WriteLine(a > b);
+            Console.WriteLine(a < b);
+            Console.WriteLine(a >= b);
+            Console.WriteLine(a <= b);
+
+            Console.ReadLine();
+        }
+    }
+}
