d30fbe47fc85a19f21fc4f07dc87ad0f4c492ab0
PinkDeer <andreyzinovets@gmail.com>
2019-11-30 11:19:41 +0300

    Update for

--- for/Program.cs
+++ for/Program.cs
@@ -8,12 +8,79 @@ namespace @for
         {
             Console.Title = "Цикл for";
 
-            for (int i = 0; i < 10; i++)
+            // for
+
+            int limit = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < limit; i++)
             {
                 Console.WriteLine(i);
             }
 
-            Console.ReadLine();
+            // Параметры не обязательны
+
+            //for (;;)
+            //{
+            //    Console.WriteLine("...");
+            //    System.Threading.Thread.Sleep(300);
+            //    break;
+            //}
+
+
+            // Без первого параметра
+
+            //int a = 0;
+
+            //for (; a < 3; a++)
+            //{
+            //    Console.WriteLine("for_1: " + a);
+            //}
+            //for (; a < 5; a++)
+            //{
+            //    Console.WriteLine("for_2: " + a);
+            //}
+
+            // Без третьего параметра
+
+            //for (int x = 0; x < 5;)
+            //{
+            //    x++;
+            //    Console.WriteLine(x);
+            //    //x++;
+            //}
+
+            // Несколько переменных
+
+            //for (int i = 0, j = 5; i < 10; i++, j++)
+            //{
+            //    Console.WriteLine("i: " + i);
+            //    Console.WriteLine("j: " + j);
+            //}
+
+            // Несколько условий
+
+            //for (int i = 0, j = 5; i < 10 && j < 12; i++, j++)
+            //{
+            //    Console.WriteLine("i: " + i);
+            //    Console.WriteLine("j: " + j);
+            //}
+
+            // В обратом порядке
+
+            //for (int i = int.Parse(Console.ReadLine()); i > 0; i--) // допустим запрос из первого параметра, если есть необходимость в ограничение видимости
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // Всё вместе
+
+            //for (int i = 0, j= 5; i < 5; i++, j--)
+            //{
+            //    Console.WriteLine("i: " + i);
+            //    Console.WriteLine("j: " + j);
+            //}
+
+            //Console.ReadLine();
         }
     }
 }
