2ed3493caf98664533030922aa1a87074ec63bed
PinkDeer <andreyzinovets@gmail.com>
2019-11-30 10:37:29 +0300

    Add for

---
+++ for/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace @for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Цикл for";
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
