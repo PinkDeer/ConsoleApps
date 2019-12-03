aac0aaeace7225828a7e837c0051903fe94fa94e
PinkDeer <andreyzinovets@gmail.com>
2019-12-03 11:13:55 +0300

    Update NET

--- NestedLoop/Program.cs
+++ NestedLoop/Program.cs
@@ -26,8 +26,8 @@ namespace NestedLoop
             Console.ReadLine();
 
             // Нарисовать многоугольник
-
-            /*            Console.Write("Введите высоту многоугольника: ");
+            /*
+                        Console.Write("Введите высоту многоугольника: ");
                         int height = int.Parse(Console.ReadLine());
 
                         Console.Write("Введите ширину многоугольника: ");
@@ -44,8 +44,108 @@ namespace NestedLoop
                             Console.WriteLine();
                         }
 
-                        Console.ReadLine();*/
+                        Console.ReadLine();
+            */
 
         }
+
+
+
+        // Пример 1 с вложеным массивом
+
+        static int[,] GetRandomArray(int rows, int columns)
+        {
+            Random random = new Random();
+
+            var result = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    result[i, j] = random.Next(100);
+                }
+
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int rows = 5;
+            int columns = 10;
+            var myArray = GetRandomArray(rows, columns);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{myArray[i, j]}\t");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+
+        // Пример 2 с вложенным массивом
+/*
+        static void Main(string[] args)
+        {
+            var gamesInfo = GetGamesInfo();
+
+            for (int i = 0; i < gamesInfo.Count; i++)
+            {
+                Console.WriteLine($"{gamesInfo[i].TypeOfGame}:");
+
+                for (int j = 0; j < gamesInfo[i].Games.Count; j++)
+                {
+                    Console.WriteLine($"{gamesInfo[i].Games[j].Name}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        static List<GameInfo> GetGamesInfo()
+        {
+            return new List<GameInfo>()
+            {
+                new GameInfo()
+                {
+                    TypeOfGame = GameType.RPG,
+                    Games = new List<Game>()
+                    {
+                        new Game(){Name = "Fallout 4"},
+                        new Game(){Name = "Gothic 2"},
+                        new Game(){Name = "The Elder Scrolls 3: Morrowind"}
+                    }
+                },
+                new GameInfo()
+                {
+                    TypeOfGame = GameType.Shooter,
+                    Games = new List<Game>()
+                    {
+                        new Game(){Name = "CS"},
+                        new Game(){Name = "BF 2"},
+                        new Game(){Name = "Doom"}
+                    }
+                },
+                new GameInfo()
+                {
+                    TypeOfGame = GameType.Strategy,
+                    Games = new List<Game>()
+                    {
+                        new Game(){Name = "Civilization VI"},
+                        new Game(){Name = "Rome: Total War"},
+                        new Game(){Name = "Starcraft II"}
+                    }
+                }
+            };
+        }
+*/
     }      
 }
