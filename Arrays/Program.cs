3f874c75d2aebee5bf36815cb3e542f68168b7d4
PinkDeer <andreyzinovets@gmail.com>
2019-12-03 20:37:42 +0300

    Add Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Массивы";
+
+            int[] myArray;
+
+            myArray = new int[5];   
+
+            Console.ReadLine();
+        }
+    }
+}
