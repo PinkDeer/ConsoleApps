abacb1a0a8b8defeb666f0e04c23b4ffea03dbd7
PinkDeer <andreyzinovets@gmail.com>
2019-12-04 16:51:34 +0300

    Update Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -8,9 +8,17 @@ namespace Arrays
         {
             Console.Title = "Массивы";
 
-            int[] myArray;
+            int[] myArray; // объяление массива
 
-            myArray = new int[5];   
+            myArray = new int[5];  // выделение места в оперативной памяти
+             
+            myArray[1] = 4; // задать значение элементу массива
+
+            int a = myArray[1]; // присвоить переменной значения элемента из массива
+
+            Console.WriteLine(myArray[1]);
+            Console.WriteLine(a);
+            Console.WriteLine(myArray.Length);
 
             Console.ReadLine();
         }
