63397aea62af919930acddd88f31517b74ca9dff
PinkDeer <andreyzinovets@gmail.com>
2019-12-04 17:15:46 +0300

    Update  Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Arrays
 {
@@ -19,6 +20,17 @@ namespace Arrays
             Console.WriteLine(myArray[1]);
             Console.WriteLine(a);
             Console.WriteLine(myArray.Length);
+
+            // Способы инициализации массива
+
+            int[] myArrayTwo = new int[5]; // В одну строку. Не допустимо не указывать сколько элементов 
+            int[] myArrayThree = new int[5] {1, 5, 75, 2, 6 };  /* 1-ый способ добавления элементов.
+            Необходимо указать столько элементов, сколько указано при выделении памяти. При добавлении элементов можно опустить второй int:
+            int[] myArrayThree = new [5] {1, 5, 75, 2, 6 };*/
+            int[] myArrayFour = new int[] { 1, 5, 75}; // 2-ой способ добавления элементов. Можно указывать сколько угодно элементов 
+            int[] myArrayFive =  { 1, 5, 75 }; // При явном указывании элементов в массиве - сокращенная записть
+            int[] myArraySix = Enumerable.Repeat(4, 10).ToArray(); // Добавить 10 элементов со значение 4
+            int[] myArraySeven = Enumerable.Range(3,5).ToArray(); // 5 элементов, первое значение 3
 
             Console.ReadLine();
         }
